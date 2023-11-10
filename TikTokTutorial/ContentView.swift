81f5e85454711cc2f9582f22adf794db42680501
ryan-cho-i <chlqudtn347@gmail.com>
2023-11-09 22:17:38 -0500

    Initial Commit

---
+++ TikTokTutorial/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TikTokTutorial
+//
+//  Created by soo on 11/9/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TikTokTutorial/TikTokTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  TikTokTutorialApp.swift
+//  TikTokTutorial
+//
+//  Created by soo on 11/9/23.
+//
+
+import SwiftUI
+
+@main
+struct TikTokTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
