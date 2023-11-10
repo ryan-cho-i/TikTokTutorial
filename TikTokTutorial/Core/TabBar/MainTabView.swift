92db3d60390a89664f38a182d832bc69e18ed91b
ryan-cho-i <chlqudtn347@gmail.com>
2023-11-09 23:12:27 -0500

    Feed View

--- TikTokTutorial/Core/TabBar/MainTabView.swift
+++ TikTokTutorial/Core/TabBar/MainTabView.swift
@@ -14,7 +14,7 @@ struct MainTabView: View {
     var body: some View {
         TabView{
             
-            Text("Feed")
+            FeedView()
                 .tabItem {
                     VStack {
                         Image(systemName: selectedTab == 0 ? "house.fill" : "house")
